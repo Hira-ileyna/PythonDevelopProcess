64006f35b2f6ad7b835943f1d52ef3fe69922994
Hiranur İleyna Bilir <hiranurbilir0@gmail.com>
2026-08-07 14:45:28 +0300

    Updated

---
+++ while-loop-implementation.py
@@ -0,0 +1,35 @@
+#1-
+# startNum = int(input("Please enter your beginning number: "))
+# stopNum = int(input("Please enter your stop number: "))
+# i = startNum
+# while(i < stopNum):
+#     if i % 2 == 0:
+#         print(i)
+#     i += 1
+
+
+#2-
+# x = 100
+# while x >= 1:
+#     print(x)
+#     x -= 1
+
+
+#3-
+
+# i = 0 
+# numbers = []
+# while i < 5:
+#     number = int(input("Number: "))
+#     numbers.append(number)
+#     i+=1
+
+# numbers.sort()      # sıralama için kullanabileceğimiz bir methodtur. Önemli!
+# print(numbers)
+
+#4-
+username = ""
+while not username:
+    username = input("Kullamıcı adı: ")
+print("girilen username " + username)
+    
