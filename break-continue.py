36d6692d123e23da3c88d8c02e4d057851cb874c
Hiranur İleyna Bilir <hiranurbilir0@gmail.com>
2026-08-10 15:33:31 +0300

    Updated

---
+++ break-continue.py
@@ -0,0 +1,17 @@
+# name = "Hiro Bit"
+
+# for i in name:
+#     if( i == "r"):
+#         break
+#     print(i)
+
+
+i = 0
+sum =  0
+while( i<= 10):
+    i +=1
+    if (i % 2 == 0):
+        continue
+    sum += i
+print(sum)
+    
