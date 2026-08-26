1a2c884cf543a4775b0e54c26ada42d97c65c8f0
Hiranur İleyna Bilir <hiranurbilir0@gmail.com>
2026-08-26 16:44:18 +0300

    Updated

---
+++ function-defining.py
@@ -0,0 +1,20 @@
+print("Hello BTK Akademi! ")
+print("Hello BTK Akademi! ")
+print("Hello BTK Akademi! ")
+print("Hello BTK Akademi! ")
+
+def greeting():
+    print("Hello BTK Akademi! ")
+
+
+greeting()
+
+for i in range(10):
+    greeting()
+
+a=10
+b=20
+def addition():
+    print(a+b)
+
+addition()
